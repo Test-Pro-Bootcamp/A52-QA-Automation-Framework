d39fd7cef43683393666a1eb216f3b2e983c1e94
shavkatsalikhov <156858200+shavkatsalikhov@users.noreply.github.com>
2024-01-23 13:49:16 -0500

    Logging in

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,40 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.remote.http.Route.options;
+
+public class Homework16 extends BaseTest {
+
+    @Test
+    public void RegistrationNavigation() {
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins*");
+
+            WebDriver driver = new ChromeDriver (options);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            String url = "https://qa.koel.app";
+            driver.get(url);
+            WebElement logo = driver.findElement(By.cssSelector(".logo"));
+            WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+            WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+            WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
+            emailInput.sendKeys("demo@class.com");
+            passwordInput.sendKeys("te$t$tudent");
+            loginButton.click();
+
+            driver.quit();
+
+        }
+    }
+
+
