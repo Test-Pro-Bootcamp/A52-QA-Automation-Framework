19b610ad8a19fedb92c2d5e6258d499520eec87b
shavkatsalikhov <156858200+shavkatsalikhov@users.noreply.github.com>
2024-01-28 22:25:40 -0500

    HW17

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,9 +1,8 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 import javax.swing.*;
 import java.time.Duration;
@@ -29,12 +28,19 @@ public class Homework17 extends BaseTest {
         passwordInput.sendKeys("te$t$tudent");
         loginButton.click();
 
+        WebElement SearchSong = driver.findElement(By.cssSelector("div[id='searchForm']"));
+        SearchSong.sendKeys("Pluto");
+        loginButton.click();
         WebElement AllSongs = driver.findElement(By.cssSelector("//*[@id='sidebar']/section[1]/ul/li[3]/a"));
         loginButton.click();
+
         WebElement AddSong = driver.findElement(By.cssSelector(".btn-add-to"));
+        driver.findElement(By.id("Value")).sendKeys(Keys.ENTER);
         JButton btn1 = new JButton ("Click me!");
         WebElement NewPlayList = driver.findElement(By.cssSelector("//*[@id='playlistWrapper']/header/div[3]/div/section[2]/form/input"));
         NewPlayList.sendKeys("Shavkat");
+        WebElement AddSelectedSong = driver.findElement(By.xpath("//*[@id='songsWrapper']/header/div[3]/span/button[2]"));
+        driver.findElement(By.id("Value")).sendKeys(Keys.ENTER);
 
         Assert.assertTrue(NewPlayList.isDisplayed());
         driver.quit();
