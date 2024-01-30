fd1f068d270da5ec972cdaf3a53eaf022df82cd3
shavkatsalikhov <156858200+shavkatsalikhov@users.noreply.github.com>
2024-01-29 23:09:44 -0500

    HW17

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,27 +1,48 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 
 public class BaseTest {
 
-    private ChromeDriver ChromeDriver;
+    protected WebDriver driver = null;
+
+    @BeforeSuite
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
 
     @BeforeMethod
     public void setUpDriver() {
-
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
-        ChromeDriver = new ChromeDriver(options);
-        WebDriver driver = null;
-        WebDriver.Timeouts timeouts = driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://qa.koel.app";
+        String url = "https://qa.koel.app/";
         driver.get(url);
     }
+    @Test
+    public void login(String email, String password) {
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
+        emailInput.sendKeys(email);
+        passwordInput.sendKeys(password);
+        loginButton.click();
+
+    //@AfterMethod
+//    public void closeBrowser() {
+//        driver.quit();
+//    }
+
+    }
+
     }
