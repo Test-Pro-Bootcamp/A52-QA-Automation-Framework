8a43bfdf8994abdcc41bcca9fe6bc1650dcfcf35
YelenaF2125 <yelena@filenkov.com>
2024-01-29 21:17:12 -0800

    hw16

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -19,6 +20,7 @@ public class LoginTests extends BaseTest {
 
         String url = "https://testpro.io/";
         driver.get(url);
+
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
     }
