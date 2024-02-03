af991056ee2928ea9e0541c081a73b5ef068763b
gavrillina <tanike18@gmail.com>
2024-02-03 09:42:35 +0600

    add actions

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
@@ -13,6 +14,7 @@ public class BaseTest {
 
     protected WebDriver driver = null;
     protected WebDriverWait wait = null;
+    protected Actions actions = null;
 
     @BeforeSuite
     static void setupClass() {
@@ -33,6 +35,7 @@ public class BaseTest {
 //        String url = "https://qa.koel.app/";
         driver.get(url);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        actions = new Actions(driver);
     }
 
     @AfterMethod
---
+++ src/test/java/PlaySongByActionsTest.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PlaySongByActionsTest extends BaseTest {
+
+    @Test
+    public void playSongByContextClickTest() {
+        String songLocator = "//section[@id='songsWrapper']//td[text()='%s']";
+        String songName = "M33 Project - Emotional Soundtrack";
+        login("demo@class.com", "te$t$tudent");
+        WebElement allSongsMenuItem = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[id='sidebar'] .songs")));
+        allSongsMenuItem.click();
+        WebElement song = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(String.format(songLocator, songName))));
+        actions.contextClick(song).perform();
+        WebElement playButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[ data-testid='song-context-menu'] .playback")));
+        playButton.click();
+        WebElement soundBar = driver.findElement(By.cssSelector("[data-test='soundbars']"));
+        Assert.assertTrue(soundBar.isDisplayed());
+    }
+
+    @Test
+    public void playSongByHoverMouseTest() {
+        login("demo@class.com", "te$t$tudent");
+        WebElement allSongsMenuItem = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[id='sidebar'] .songs")));
+        allSongsMenuItem.click();
+        WebElement mediaPlayer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#mainFooter .play")));
+        actions.moveToElement(mediaPlayer).click().perform();
+        WebElement playButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#mainFooter .play i")));
+        playButton.click();
+        WebElement soundBar = driver.findElement(By.cssSelector("[data-test='soundbars']"));
+        Assert.assertTrue(soundBar.isDisplayed());
+    }
+}
