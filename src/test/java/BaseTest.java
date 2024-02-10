767254cec075fa07e016d57022c6f59730e2048d
gavrillina <tanike18@gmail.com>
2024-02-10 09:16:41 +0600

    add selenium grid

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,9 +5,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class BaseTest {
@@ -16,22 +21,11 @@ public class BaseTest {
     protected WebDriverWait wait = null;
     protected Actions actions = null;
 
-    @BeforeSuite
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-
     @BeforeMethod
     @Parameters("baseUrl")
-    public void setUpDriver(String url) {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-notifications");
-
-        driver = new ChromeDriver(options);
+    public void setUpDriver(String url) throws MalformedURLException {
+        driver = pickDriver(System.getProperty("browser"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
 //        String url = "https://qa.koel.app/";
         driver.get(url);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -41,5 +35,29 @@ public class BaseTest {
     @AfterMethod
     public void closeBrowser() {
         driver.quit();
+    }
+
+    public WebDriver pickDriver(String browser) throws MalformedURLException {
+        String gridUrl = "http://192.168.1.220:4444/";
+        ChromeOptions options = new ChromeOptions();
+        switch (browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--disable-notifications");
+                return driver = new ChromeDriver(options);
+            case "safari":
+                WebDriverManager.safaridriver().setup();
+                return driver = new SafariDriver();
+            case "grid":
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                capabilities.setCapability("browserName", "safari");
+                return driver = new RemoteWebDriver(URI.create(gridUrl).toURL(), capabilities);
+            default:
+                WebDriverManager.chromedriver().setup();
+                options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--disable-notifications");
+                return driver = new ChromeDriver(options);
+        }
     }
 }
