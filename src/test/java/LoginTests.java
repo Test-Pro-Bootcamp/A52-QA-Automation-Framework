804345a73c986a47b6b23da43c57024d814ebebd
shavkatsalikhov <156858200+shavkatsalikhov@users.noreply.github.com>
2024-02-15 12:28:38 -0500

    Merge pull request #13 from shavkatsalikhov/Homework18

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -17,18 +17,3 @@ public void loginTest() {
     Assert.assertTrue(avatar.isDisplayed()); //true
 }
 }
-
-//@Test
-//    public void
-//    ChromeOptions options = new ChromeOptions();
-//    options.addArguments("--remote-allow-origins=*");
-
-//    WebDriver driver = new ChromeDriver(options);
-//    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-//    String url = "https://qa.koel.app/";
-//    driver.get(url);
-//    Assert.assertEquals(driver.getCurrentUrl(), url);
-//    driver.quit();
-
-
