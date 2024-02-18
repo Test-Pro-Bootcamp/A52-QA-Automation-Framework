3115aac380fe85c11bc98720e48bc1d12c4c3480
shavkatsalikhov <156858200+shavkatsalikhov@users.noreply.github.com>
2024-02-18 15:28:09 -0500

    Merge pull request #13 from shavkatsalikhov/Homework18

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.time.Duration;
+
 
 public class LoginTests extends BaseTest {
 @Test(description = "Check if user logs in with correct credentials", priority = 1, groups = "Smoke")
