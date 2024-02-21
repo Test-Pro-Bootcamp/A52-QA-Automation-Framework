b8cc7f15c9dc87ad4920e03c271a24e01215c69b
Dzzmitry <dzmitry.kimber@testpro.io>
2024-02-21 20:53:56 +0300

    16

--- src/test/java/Homwork16.java
+++ src/test/java/Homwork16.java
@@ -7,11 +7,11 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.time.Duration;
 
-public class Homwork16 {
+public class Homwork16 extends BaseTest {
 
     @Test
 
-    public <registrationLink> void  registrationNavigationTest() {
+    public void  registrationNavigation() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
@@ -20,10 +20,14 @@ public class Homwork16 {
         String url = "https://qa.koel.app/";
         driver.get(url);
         WebElement registrationLink = driver.findElement(By.cssSelector("[href = 'registration']"));
-        registrationLink click ;
-        WebElement registrationText = driver.findElement(By.xpath("//h2[contains(text(),'Register new account or']"));
-        Assert.assertTrue(registrationText.isDisplayed());
+        registrationLink click ();
+        String registrationUrl = "https://qa.koel.app/registration";
+        Assert.assertEquals(driver.getCurrentUrl(),registrationUrl);
         driver.quit();
+
+       /* WebElement registrationText = driver.findElement(By.xpath("//h2[contains(text(),'Register new account or']"));
+        Assert.assertTrue(registrationText.isDisplayed());
+        driver.quit();*/
 
 
     }
