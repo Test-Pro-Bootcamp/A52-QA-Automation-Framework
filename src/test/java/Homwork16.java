a161d8aa5fe521175a0701f734420a731df7553e
Dzzmitry <dzmitry.kimber@testpro.io>
2024-02-21 13:17:36 +0300

    testcase1

--- src/test/java/Homwork16.java
+++ src/test/java/Homwork16.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Homwork16 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+public class Homwork16 {
+    @Test
+   public void registrationNavigation() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver() ;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(),url);
+        driver.quit();
+
+    }
 }
+
+
